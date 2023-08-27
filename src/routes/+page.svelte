8109465c89fd085c03246b1d6fd323d809f26e33
Jay D Jackson <jj@jj.is-a.dev>
2023-08-27 03:52:59 +0000

    init w/shaka

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,30 @@
-hbnc
+<script lang="ts">
+	import { getHLS } from '$lib/dl-music';
+	import { onMount } from 'svelte';
+
+	let video: HTMLVideoElement;
+
+	onMount(async () => {
+		const { default: shaka } = await import('shaka-player');
+
+		if (!shaka.Player.isBrowserSupported()) {
+			alert('Unsupported browser.');
+			throw new Error('unsupported browser.');
+		}
+
+		const HLS = await getHLS('6_aqeCDk1Yk');
+
+		const player = new shaka.Player(video);
+
+		await player.load(HLS);
+	});
+</script>
+
+<svelte:head>
+	<script src="./mux.min.js" async></script>
+</svelte:head>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<video bind:this={video} controls autoplay />
+
+<h1>Spiffy</h1>
