7de1fd37d6df7578715c84abf73566e86dcdcda1
Jay Jackson <jj@jj.is-a.dev>
2023-08-28 13:19:16 -0700

    add base path

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,13 +1,14 @@
 import { browser } from '$app/environment';
 import type { SpotifyApi } from '@spotify/web-api-ts-sdk';
 import { writable } from 'svelte/store';
+import { base } from '$app/paths';
 
 export const CLIENT_ID = browser ? localStorage.getItem('clientID') : null;
 export const CLIENT_SECRET = browser ? localStorage.getItem('clientSecret') : null;
 export const TOKEN = browser ? localStorage.getItem('token') : null;
 
 export const REDIRECT_URI = browser
-	? `${window.location.origin}/callback`
+	? `${window.location.origin}${base}/callback`
 	: 'https://example.com/callback';
 
 export const spotifySDK = writable<SpotifyApi | null>();
