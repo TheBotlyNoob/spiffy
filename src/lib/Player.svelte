50fa78bdcb6fd48e9ee86c45c0cb305261496d5d
Jay D Jackson <jj@jj.is-a.dev>
2023-08-28 18:06:29 +0000

    working player

--- src/lib/Player.svelte
+++ src/lib/Player.svelte
@@ -1,42 +1,56 @@
 <script lang="ts">
 	import { spotifySDK } from '$lib/spotify';
 	import { PUBLIC_INVIDIOUS_INSTANCE } from '$env/static/public';
-
-	let tracksElem: HTMLOListElement | null;
+	import type { Track } from '@spotify/web-api-ts-sdk';
+	import { Howl, Howler } from 'howler';
 
 	$: tracks = $spotifySDK?.currentUser.tracks.savedTracks();
 
-	$: {
-		tracks?.then((tracks) => {
-			for (const child of tracksElem?.children as unknown as HTMLElement[]) {
-				const { trackName, artist } = child.dataset;
-				if (!trackName || !artist) {
-					continue;
-				}
-				(async () => {
-					// await fetch(
-					// 	PUBLIC_INVIDIOUS_INSTANCE +
-					// 		'/api/v1/search?' +
-					// 		new URLSearchParams({
-					// 			q: trackName + ' ' + artist
-					// 		})
-					// )
-					// 	.then((r) => r.json())
-					// 	.then((r) => console.log(r));
-				})();
+	const onClick = (track: Track): ((e: MouseEvent) => Promise<void>) => {
+		return async (e) => {
+			interface Results {
+				type: string;
+				title: string;
+				videoId: string;
 			}
-		});
-	}
+
+			const results: Results[] = await fetch(
+				PUBLIC_INVIDIOUS_INSTANCE +
+					'/api/v1/search?' +
+					new URLSearchParams({
+						q: track.name + ' ' + track.artists[0].name
+					})
+			)
+				.then((r) => r.json())
+				.then((r: Results[]) => r.filter((v) => v.type === 'video'));
+
+			let sound = new Howl({
+				src: [
+					PUBLIC_INVIDIOUS_INSTANCE +
+						'/latest_version?' +
+						new URLSearchParams({
+							id: results[0].videoId,
+							itag: '139'
+						})
+				],
+				html5: true
+			});
+			sound.play();
+			alert('clicked');
+		};
+	};
 </script>
 
 {#await tracks}
 	<p>loading tracks...</p>
 {:then tracks}
 	{#if tracks}
-		<ol bind:this={tracksElem}>
-			{#each tracks.items as track}
-				<li data-track-name={track.track.name} data-artist={track.track.artists[0].name}>
-					{track.track.name} - {track.track.artists[0].name}
+		<ol>
+			{#each tracks.items as { track }}
+				<li>
+					<button class="btn btn-active" on:click={onClick(track)}>
+						{track.name} - {track.artists[0].name}
+					</button>
 				</li>
 			{/each}
 		</ol>
